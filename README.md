b4a9d180d0671f48b09afd11789033facdb79675
USER <user@AL01457295.local>
2020-12-30 16:17:57 +0900

    server v3

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # TWITTER SENTIMENT TRENDER
 
 Graph sentiment trends by topic on twitter and compare with other topics.
+
+## Server
+
+/api/search/:query
+
+returns 
+
+```json
+{
+    "2020-12-29T07:08:42.485Z" :{
+        "statusCode": 200,
+        "body":{
+            "data":[
+                {
+                    "id": "1279991360007929856",
+                    "lang": "en",
+                    "text": "RT @the_yellow_fall: steady v3.1.13 releases: Discover, assess and mitigate known vulnerabilities in your Java and Python projects https://…"
+                },
+                {},
+                {},
+            ],
+            "meta":{}
+        },
+        "headers":{},
+        "request":{}
+    },
+    "2020-12-28T07:08:42.485Z" :{},
+    "2020-12-27T07:08:42.485Z" :{},
+    "2020-12-26T07:08:42.485Z" :{},
+    "2020-12-25T07:08:42.485Z" :{},
+    "2020-12-24T07:08:42.485Z" :{},
+    "2020-12-23T07:08:42.485Z" :{}
+}
+```
+
+or 
+
+Error Msg
--- server/server.js
+++ server/server.js
@@ -38,43 +38,133 @@ const authMessage = {
 };
 
 
+// app.get("/api/search/:query", async (req, res) => {
+//   if (!BEARER_TOKEN) {
+//     res.status(400).send(authMessage);
+//   }
+
+//   const token = BEARER_TOKEN;
+
+//   var finalResponse = {}
+//   var now = new Date();
+
+//   var start = new Date();
+//   var end = new Date();
+//   start.setDate(now.getDate()-5);
+//   end.setDate(now.getDate()-4);
+
+//   const requestConfig = {
+//     url: searchURL + "?query=" + req.params.query + "&end_time=" + end.toISOString() + "&start_time=" + start.toISOString() + "&tweet.fields=lang",
+//     auth: {
+//       bearer: token,
+//     },
+//     json: true,
+//   };
+
+//   try {
+//     const response = await get(requestConfig);
+
+//     if (response.statusCode !== 200) {
+//       if (response.statusCode === 403) {
+//         res.status(403).send(response.body);
+//       } else {
+//         throw new Error(response.body.error.message);
+//       }
+//     }
+
+//     res.send(response);
+//   } catch (e) {
+//     res.send(e);
+//   }
+// });
+
+
 app.get("/api/search/:query", async (req, res) => {
   if (!BEARER_TOKEN) {
     res.status(400).send(authMessage);
   }
 
   const token = BEARER_TOKEN;
+  const url = searchURL + "?query=" + req.params.query + "&tweet.fields=lang"
 
-  var now = new Date();
-  var start = new Date();
-  var end = new Date();
-  start.setDate(now.getDate()-5);
-  end.setDate(now.getDate()-4);
+  var finalResponse = {}
 
-  const requestConfig = {
-    url: searchURL + "?query=" + req.params.query + "&end_time=" + end.toISOString() + "&start_time=" + start.toISOString() + "&tweet.fields=lang",
-    auth: {
-      bearer: token,
-    },
-    json: true,
-  };
+  var startTime = new Date();
+  var endTime = new Date();
+  var count = 0; 
 
-  try {
-    const response = await get(requestConfig);
+  while (count < 7) {
 
-    if (response.statusCode !== 200) {
-      if (response.statusCode === 403) {
-        res.status(403).send(response.body);
-      } else {
-        throw new Error(response.body.error.message);
+    startTime.setDate(endTime.getDate()-1)
+    var startTimeString = startTime.toISOString()
+    
+    if (count === 0) {
+
+      const requestConfig = {
+        url: url + "&start_time=" + startTimeString,
+        auth: {
+          bearer: token,
+        },
+        json: true,
+      };
+    
+      try {
+        const response = await get(requestConfig);
+    
+        if (response.statusCode !== 200) {
+          if (response.statusCode === 403) {
+            res.status(403).send(response.body);
+          } else {
+            throw new Error(response.body.error.message);
+          }
+          break;
+        }
+    
+        finalResponse[startTimeString] = response;
+
+      } catch (e) {
+        res.send(e);
+        break;
       }
+
+    } else {
+
+      const requestConfig = {
+        url: url + "&start_time=" + startTimeString + '&end_time=' + endTime.toISOString(),
+        auth: {
+          bearer: token,
+        },
+        json: true,
+      };
+    
+      try {
+        const response = await get(requestConfig);
+    
+        if (response.statusCode !== 200) {
+          if (response.statusCode === 403) {
+            res.status(403).send(response.body);
+          } else {
+            throw new Error(response.body.error.message);
+          }
+          break;
+        }
+    
+        finalResponse[startTimeString] = response;
+
+      } catch (e) {
+        res.send(e);
+        break;
+      }
+
     }
 
-    res.send(response);
-  } catch (e) {
-    res.send(e);
+    endTime.setDate(startTime.getDate());
+    count++;
   }
+
+  res.send(finalResponse)
 });
+
 
 
 console.log("NODE_ENV is", process.env.NODE_ENV);
